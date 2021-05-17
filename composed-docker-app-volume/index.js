1ba7ac430339727071cc960fd11d687c2d594c9f
alefais <alessandra.fais@gmail.com>
2021-05-17 19:19:53 +0200

    Added first check on the validity of the visits counter value retrieved from redis-db

---
+++ composed-docker-app-volume/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const redis = require('redis')
+
+const app = express()
+const client = redis.createClient({
+	host: 'redis-db',
+	port: 6379
+})
+
+client.exists('visits', (err, reply) => {
+    if (reply === 0)
+        client.set('visits', 0);
+})
+
+app.get('/', (req, res) => {
+    client.get('visits', (err, visits) => {
+        client.set('visits', parseInt(visits) + 1)
+        res.send('Number of visits is: ' + visits)
+    })
+})
+
+app.listen(8081, ()=>{
+    console.log('Listening on port 8081')
+})
